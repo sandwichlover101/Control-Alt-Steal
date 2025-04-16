a80d1e8bf933aa383309b5bc591fb1d1dc522d63
sandwichlover101 <slange26@belmontschools.net>
2025-04-16 10:40:01 -0400

    Update Level4.js

--- Level4.js
+++ Level4.js
@@ -5,3 +5,13 @@ function clue1() {
 function clue2() {
 
 }
+
+function criminal() {
+  let guess= prompt("Type in who is guilty");
+  if (guess=="Nina Narc") {
+    alert("Yay you guessed correctly");
+  }
+  else {
+    alert("wrong");
+  }
+}
