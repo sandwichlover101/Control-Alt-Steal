61b4fa946644c61d13ad1c78baba885ef215f827
sandwichlover101 <slange26@belmontschools.net>
2025-04-29 11:53:59 -0400

    Create Level5.js

---
+++ Level5.js
@@ -0,0 +1,6 @@
+function autopsy () {
+  document.write("<ul> <li>Garry officially died at 11:08pm</li>
+<li>Body has been at the lake for almost 5 hours</li>
+<li>No cuts or stab wounds</li>
+<li>Died due to toxic poison → made at home very simple</li> </ul>"
+}
