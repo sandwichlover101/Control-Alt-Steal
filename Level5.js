ec9abe8259b53aa30955a55bada97cf4b5a11f28
sandwichlover101 <slange26@belmontschools.net>
2025-04-29 11:59:06 -0400

    Update Level5.js

--- Level5.js
+++ Level5.js
@@ -1,6 +1,6 @@
 function autopsy () {
-  document.write("<ul> <li>Garry officially died at 11:08pm</li>
-<li>Body has been at the lake for almost 5 hours</li>
-<li>No cuts or stab wounds</li>
-<li>Died due to toxic poison → made at home very simple</li> </ul>"
+  alert(`Garry officially died at 11:08pm
+Body has been at the lake for almost 5 hours
+No cuts or stab wounds
+Died due to toxic poison → made at home very simple`);
 }
