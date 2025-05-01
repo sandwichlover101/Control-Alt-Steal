9ac6b432469a22ea3fa056b1ca2cc34d1cc34288
sandwichlover101 <slange26@belmontschools.net>
2025-05-01 11:34:49 -0400

    Update Level5.js

--- Level5.js
+++ Level5.js
@@ -8,9 +8,3 @@ function criminal() {
   }
 }
 
-function autopsy () {
-  alert(`Garry officially died at 11:08pm
-Body has been at the lake for almost 5 hours
-No cuts or stab wounds
-Died due to toxic poison → made at home very simple`);
-}
