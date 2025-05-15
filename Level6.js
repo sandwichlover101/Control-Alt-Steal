09039ea098023e041b4c5bd05ba24f7a0086583f
sandwichlover101 <slange26@belmontschools.net>
2025-05-15 11:34:30 -0400

    Update Level6.js

--- Level6.js
+++ Level6.js
@@ -1,1 +1,16 @@
+function criminal() {
+  let guess= prompt("Type in who is guilty");
+  if (guess=="Stumble Door and Williams Dej") {
+    alert("Yay congrats on solving the case");
+  }
+  else {
+    alert("wrong");
+  }
+}
+
+function conclusion() {
+alert("It was Williams Dej, as he was hacking the security cameras of the Dons mansion, he did this because he was u>
+}
+
+
 
