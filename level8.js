ba6e3ff0e5abcc33be00de81d298124b2d5b6c00
pinnapple03 <lcadet27@belmontschools.net>
2025-05-21 11:21:51 -0400

    Create level8.js

---
+++ level8.js
@@ -0,0 +1,10 @@
+function criminal() {
+  let guess= prompt("Type in who is guilty");
+  if (guess=="Christine Crow") {
+    alert("Yay you guessed correctly");
+  }
+  else {
+    alert("wrong");
+  }
+}
+
