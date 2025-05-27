2f3813a789779034e00138e2e3802e96db23c160
pinnapple03 <lcadet27@belmontschools.net>
2025-05-27 11:32:44 -0400

    Update level9.js

--- level9.js
+++ level9.js
@@ -1,1 +1,11 @@
+function criminal() {
+  let guess= prompt("Type in who is guilty");
+  if (guess=="William Dej") {
+    alert("Yay you guessed correctly");
+  }
+  else {
+    alert("wrong");
+  }
+}
+
 
